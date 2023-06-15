7f4a7a2eba03169555e36b056fdfd6eca1bbee9b
Fernando Ramirez Basteres <127370858+fbasteres@users.noreply.github.com>
2023-06-15 14:02:02 -0500

    Prueba 2

--- views/pages/municipalidad.php
+++ views/pages/municipalidad.php
@@ -8,7 +8,7 @@
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Municipalidad</title>
+    <title>MPT | Municipalidad</title>
     <link rel="stylesheet" href="<?php url();?>/assets/css/main.css?=v1.1">
     <link rel="stylesheet" href="<?php url();?>/node_modules/fontawesome_6.3.0/css/all.min.css">
     <link rel="stylesheet" href="<?php url();?>/assets/css/style.css?=v1.1">
@@ -19,7 +19,7 @@
     ?>
 </header>
 <body>
-    
+    <h1>Hola prueba</h1>
 
 <script src="<?php url(); ?>/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
