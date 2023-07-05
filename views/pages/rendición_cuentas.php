0f64598b4f36e5d4cab91d8bf2b563c0705df0db
fbasteres <ferjav4351@gmail.com>
2023-07-04 19:38:39 -0500

    Nueva pagina

---
+++ views/pages/rendición_cuentas.php
@@ -0,0 +1,147 @@
+<?php
+  require '../../funciones/funciones.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MPT | CAS </title>
+    <link rel="stylesheet" href="<?php url();?>/assets/css/main.css?=v1.1">
+    <link rel="stylesheet" href="<?php url();?>/node_modules/fontawesome_6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="<?php url();?>/assets/css/style.css?=v1.5">
+    <link rel="stylesheet" href="<?php url();?>/assets/css/cas.css">
+    
+</head>
+<header>
+    <?php
+    include '../webcomponents/header.php'
+    ?>
+</header>
+<body>
+    <div class="bg_pag">
+      <main class="fond_pag">
+        <div class="container col-xxl-12 gutter">
+          <div class="row flex-lg-row-reverse align-items-center">
+            <div class="col-10 col-sm-8 col-lg-6"> </div>
+              <div class="col-lg-6 pag_container">
+                <h4 class="">Rendición de cuentas</h4>
+                <p class="">Régimen Especial de Contratación Administrativa de Servicios (CAS) - D.Leg Nº 1057.</p>
+              </div>
+          </div>
+        </div>
+      </main>
+    </div>
+
+
+    <div class="container">
+        <h3 class="pt-5 fw-bold fs-5 h3-cas">Instrucciones a los Postulantes</h3>
+        <p>El postulante que considere que cumple con los requisitos para el servicio requerido, presentará su Hoja de Vida, anexos y documentos sustentatorios, en un folder o anillado, debidamente foliado y ordenado de acuerdo a lo que se indica en las bases de la convocatoria.
+          Toda la documentación se presentará dentro de un sobre cerrado (presentar a la suscripción de contrato) y estará dirigida a la Sub Gerencia de Recursos Humanos (Att.: Comisión de Evaluación), colocando en el sobre la correspondiente etiqueta.
+        </p>
+        <h3 class="pt-2 fw-bold fs-5 h3-cas">Comunicado</h3>
+        <p>Se comunica a los interesados y al público en general que, en atención al Decreto Legislativo N° 1505, se da reinicio de los procesos de selección CAS que quedaron suspendidos en el marco del Estado de Emergencia Nacional Sanitaria.
+            Los procesos se reiniciarán en las etapas en que quedaron hasta el 16 de marzo pasado, fecha en que se publicó el Decreto Supremo N° 044-2020-PCM, y se conducirán según lo establecido en la “Guía operativa para la gestión de recursos humanos durante la emergencia sanitaria por el COVID-19”, aprobada por la Autoridad Nacional del Servicio Civil.
+            Invitamos a todos los interesados y público en general a revisar los procesos, a fin de acceder a la información respectiva.
+            Tarma, 15 de marzo de 2023
+        </p>
+    </div>
+      <main>
+        <div class="container py-4">
+          <header class="mb-4">
+              <h3 class="fw-bold h3-cas">Documentos</h3><hr>
+          </header>
+          <div class="row align-items-md-stretch">
+              
+            <div class="col-md-4 mb-4">
+              <div class="h-100 p-4 bg-secondary-subtle border rounded-3 shadow">
+                <h5 class="h4-documento">Anexos</h5>
+                <p class="text-muted">Descarga los anexos y rellena correctamente.</p>
+                <a href="https://www.cdn.munitarma.gob.pe/files/pdf/convocatoria/cas/2023/anexos_2023.pdf" target="_blank" class="href-button">
+                    <button class="btn btn-mpt-primary fw-semibold" type="button"> <i class="fa-solid fa-circle-arrow-down"></i>&nbsp &nbsp Descargar</button>
+                </a>
+              </div>
+            </div>
+            
+            <div class="col-md-4 mb-4">
+              <div class="h-100 p-4 bg-secondary-subtle border rounded-3 shadow">
+                <span class="badge rounded-pill" style="color: #3d4053;background-color: #e9eaea;">Finalizado</span>
+                <h5 class="h4-documento pt-2">Bases de convocatoria</h5>
+                <p class="text-muted">CAS 001-2023-CEE/MPT.</p>
+                <a href="https://www.cdn.munitarma.gob.pe/files/pdf/convocatoria/cas/2023/CAS_001-2023-MPT,.pdf" target="_blank" class="href-button">
+                  <button class="btn btn-mpt-primary fw-semibold " type="button"> <i class="fa-solid fa-circle-arrow-down"></i>&nbsp &nbsp Descargar</button>
+                </a>
+              </div>
+            </div>
+            
+            <div class="col-md-4 mb-4">
+              <div class="h-100 p-4 bg-secondary-subtle border rounded-3 shadow">
+                <h5 class="h4-documento">Comunicado
+                </h5>
+                <p class="text-muted"><b>Reprogramación de horario.</b><br>CAS 001-2023-CEE/MPT.</p>
+                <a href="https://www.cdn.munitarma.gob.pe/files/pdf/convocatoria/cas/2023/comunicado_cas-2023.pdf" target="_blank" class="href-button">
+                  <button class="btn btn-mpt-primary fw-semibold " type="button"> <i class="fa-solid fa-circle-arrow-down"></i>&nbsp &nbsp Descargar</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="container py-4">
+          <header class="mb-4">
+              <h3 class="fw-bold h3-cas">Resultados</h3><hr>
+          </header>
+          <div class="row align-items-md-stretch">
+              
+            <div class="col-md-4 mb-4">
+              <div class="h-100 p-4 bg-secondary-subtle border rounded-3 shadow">
+                <span class="badge rounded-pill " style="color: #126b49;background-color: #c9f0d8;">Reciente</span>
+                <h5 class="h4-documento pt-2">Comunicado</h5>
+                <p class="text-muted"><b>Reprogramación de horario.</b><br>CAS 001-2023-CEE/MPT</p>
+                <a href="https://www.cdn.munitarma.gob.pe/files/pdf/convocatoria/cas/2023/com_cas_2023.pdf" target="_blank" class="href-button">
+                  <button class="btn btn-mpt-primary fw-semibold " type="button"> <i class="fa-solid fa-circle-arrow-down"></i>&nbsp &nbsp Descargar</button>
+                </a>
+              </div>
+            </div>
+            
+            <div class="col-md-4 mb-4">
+              <div class="h-100 p-4 bg-secondary-subtle border rounded-3 shadow">
+                <span class="badge rounded-pill " style="color: #126b49;background-color: #c9f0d8;">Reciente</span>
+                <h5 class="h4-documento pt-2">Resultados preliminares </h5>
+                <p class="text-muted"><b>Evaluación de Currículum Vitae.</b><br>CAS 001-2023-CEE/MPT</p>
+                <a href="https://www.cdn.munitarma.gob.pe/files/pdf/convocatoria/cas/2023/rst_pre_1_2023.pdf" target="_blank" class="href-button">
+                    <button class="btn btn-mpt-primary fw-semibold" type="button"> <i class="fa-solid fa-circle-arrow-down"></i>&nbsp &nbsp Descargar</button>
+                </a>
+              </div>
+            </div>
+            
+            <div class="col-md-4 mb-4">
+              <div class="h-100 p-4 bg-secondary-subtle border rounded-3 shadow">
+                <span class="badge rounded-pill " style="color: #126b49;background-color: #c9f0d8;">Reciente</span>
+                <h5 class="h4-documento pt-2">Resultados preliminares</h5>
+                <p class="text-muted"><b>Examen de conocimientos.</b><br>CAS 001-2023-CEE/MPT</p>
+                <a  href="https://www.cdn.munitarma.gob.pe/files/pdf/convocatoria/cas/2023/rst_pre_2_2023.pdf" target="_blank" class="href-button">
+                  <button class="btn btn-mpt-primary fw-semibold" type="button"> <i class="fa-solid fa-circle-arrow-down"></i>&nbsp &nbsp Descargar</button>
+                </a>
+              </div>
+            </div>
+            
+            <div class="col-md-4 mb-4">
+              <div class="h-100 p-4 bg-secondary-subtle border rounded-3 shadow">
+                <span class="badge rounded-pill" style="color: #331980;background-color: #c2b1f5;">Nuevo</span>
+                <h5 class="h4-documento pt-2">Resultado Final</h5>
+                <p class="text-muted">CAS 001-2023-CEE/MPT</p>
+                <a  href="https://www.cdn.munitarma.gob.pe/files/pdf/convocatoria/cas/2023/rst_final_2023.pdf" target="_blank" class="href-button">
+                  <button class="btn btn-mpt-primary fw-semibold" type="button"> <i class="fa-solid fa-circle-arrow-down"></i>&nbsp &nbsp Descargar</button>
+                </a>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+
+<script src="<?php url(); ?>/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
