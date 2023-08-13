f2ceb6b1ac45ec9842b3aecd4b1773a0b63a411d
fbasteres <ferjav4351@gmail.com>
2023-08-13 03:49:02 -0500

    url-footer.css

--- views/webcomponents/footer.php
+++ views/webcomponents/footer.php
@@ -1,13 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Footer</title>
-
     <!-- Custom styles -->
-    <link rel="stylesheet" href="assets/css/footer.css?v=2.2">
+    <link rel="stylesheet" href="<?php url();?>assets/css/footer.css?v=2.2">
 </head>
 <body>
 <div class="footer pt-5">
