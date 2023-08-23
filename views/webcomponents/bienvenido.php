3e4c91e0f10e3614c3e59127b92db401f5c235a4
fbasteres <ferjav4351@gmail.com>
2023-08-23 09:57:57 -0500

    Publicar Rendicion

--- views/webcomponents/bienvenido.php
+++ views/webcomponents/bienvenido.php
@@ -11,9 +11,9 @@
               <div class="bienvenido_container col-lg-6">
                   <p><b>Audiencia pública </b> | Gestión 2023 - 2026</p>
                   <h1 class="display-5">Rendición de cuentas</h1>
-                  <p class="pt-0 normas">Aprobado con <a href="" target="_blank">Decreto de Alcaldía N°012-2023-ALC/MPT &nbsp<i class="fa-solid fa-up-right-from-square"></i></a></p>
+                  <p class="pt-0 normas">Aprobado con <a href="" target="_blank">Decreto de Alcaldía N°013-2023-ALC/MPT &nbsp<i class="fa-solid fa-up-right-from-square"></i></a></p>
                   <br>
-                  <a href="https://cdn.munitarma.gob.pe/files/pdf/rendicion_cuentas/RC_2023.pdf" target="_blank" class="href-button">
+                  <a href="https://facilita.gob.pe/t/5726" target="_blank" class="href-button">
                      <button class="btn btn-mpt-primary px-4 fw-semibold"><i class="fa-solid fa-comments"></i>&nbsp &nbsp Ingresar Pregunta</button>
                   </a>
               </div>
